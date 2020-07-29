91078bb03091d1a0f06d87f58c7738940bd70f96
Rudrasish Sarkar <61749772+MrRudraSarkar@users.noreply.github.com>
2020-07-30 00:29:18 +0530

    updating sql server

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const image = require('./controllers/image');
 const db = knex({
 	client: 'pg',
 	connection: {
-		host : '127.0.0.1',
+		host : 'postgresql-sinuous-05973',
 		user : 'postgres',
-		password : 'rudra007',
+		password : '',
 		database : 'smartbrain'
 	}
 });
